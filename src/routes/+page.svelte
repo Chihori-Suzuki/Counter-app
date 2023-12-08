7bd5cebf4465f0e24303aacdd348f04ff525c859
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2023-12-07 18:28:08 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Counter from "./Counter.svelte";
+
+  type Counter = { id: number; title: string; count: number };
+  let counters: Counter[] = [{ id: 1, title: "new", count: 0 }];
+  $: maxId = 1;
+  $: titleList = counters.map((counter) => counter.title);
+  $: sum = counters.reduce((acc, cur): number => acc + cur.count, 0);
+
+  const createCounter = (): void => {
+    let newId: number = maxId + 1;
+    counters = [...counters, { id: newId, title: "new", count: 0 }];
+    maxId += 1;
+  };
+  const deleteCounter = (id: number): void => {
+    counters = counters.filter((counter) => counter.id !== id);
+  };
+</script>
+
+<div class="flex flex-col items-center">
+  <h1 class="text-6xl m-6">Multiple Counter</h1>
+
+  {#each counters as counter (counter.id)}
+    <Counter
+      on:click={() => deleteCounter(counter.id)}
+      bind:count={counter.count}
+      bind:title={counter.title}
+    />
+  {/each}
+
+  <button
+    on:click={createCounter}
+    class="w-96 m-auto text-center bg-green-400 rounded text-white cursor-pointer"
+    >new counter</button
+  >
+  <p>title list: {titleList}</p>
+  <p>sum of count: {sum}</p>
+</div>
---
+++ src/routes/Counter.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  export let count: number = 0;
+
+  export let title: string = "new";
+  const increment = (): void => {
+    count += 1;
+  };
+  const decrement = (): void => {
+    count -= 1;
+  };
+  const clear = (): void => {
+    count = 0;
+  };
+  const changeTitle = (title: string): void => {
+    title = title;
+  };
+</script>
+
+<div
+  class="w-96 rounded border-solide bg-gray-100 shadow-lg m-auto flex flex-row justify-between mb-2 p-2"
+>
+  <input
+    bind:value={title}
+    on:change={() => changeTitle(title)}
+    class="text-gray-600 ml-4 px-1 w-32"
+  />
+  <p class="text-lg font-bold px-4 centerize">{count}</p>
+  <div class="flex">
+    <button
+      on:click={increment}
+      class="btn bg-red-400 rounded-l px-2 text-white">+</button
+    >
+    <button on:click={decrement} class="btn bg-blue-400 px-2 text-white"
+      >-</button
+    >
+    <button on:click={clear} class="btn bg-yellow-400 rounded-r px-2 text-white"
+      >0</button
+    >
+    <button on:click class="text-gray-500 cursor-pointer px-4">x</button>
+  </div>
+</div>
