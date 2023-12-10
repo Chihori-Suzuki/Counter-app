4486ef2584c185907d8741e4714a5e6d4adf753c
Chihori-Suzuki <69213233+Chihori-Suzuki@users.noreply.github.com>
2023-12-09 22:11:15 -0800

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,10 @@
   };
 </script>
 
+<svelte:head>
+  <title>Multiple Counter</title>
+</svelte:head>
+
 <div class="flex flex-col items-center">
   <h1 class="text-6xl m-6">Multiple Counter</h1>
 
